10b8d5213d08e8f448eed604f5ee7543e8563a1d
malzma <markus.malz@it2media.de>
2017-06-13 10:26:52 +0200

    Initital commit

---
+++ DryLib.Sql/DryLib.Sql/Dal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DryLib.Sql
+{
+    public class Dal
+    {
+        private readonly string _connectionString;
+
+        public Dal(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public int ExecuteNonQuery(string sqlText)
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                var sqlCommand = new SqlCommand(sqlText, sqlConnection);
+
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+        public DataTable ReadUncommitted(FileInfo sqlFile)
+        {
+            return ReadUncommitted(File.ReadAllText(sqlFile.FullName, Encoding.UTF8));
+        }
+
+        public DataTable ReadUncommitted(string sqlStatement)
+        {
+            var sqlConnection = new SqlConnection(_connectionString);
+            sqlConnection.Open();
+            var readUncommitedTransaction = sqlConnection.BeginTransaction(IsolationLevel.ReadUncommitted);
+            var dataSet = new DataSet();
+            using (sqlConnection)
+            {
+                try
+                {
+                    var sqlCommand = new SqlCommand(sqlStatement, sqlConnection) { Transaction = readUncommitedTransaction };
+                    var sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                    sqlDataAdapter.Fill(dataSet);
+
+                    readUncommitedTransaction.Commit();
+
+                    sqlConnection.Close();
+                }
+                catch
+                {
+                    readUncommitedTransaction.Rollback();
+                    throw;
+                }
+            }
+            return dataSet.Tables[0];
+        }
+    }
+}
---
+++ DryLib.Sql/DryLib.Sql/IDal.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using System.IO;
+
+namespace DryLib.Sql
+{
+    public interface IDal
+    {
+        int ExecuteNonQuery(string sqlText);
+        DataTable ReadUncommitted(FileInfo sqlFile);
+        DataTable ReadUncommitted(string sqlStatement);
+    }
+}

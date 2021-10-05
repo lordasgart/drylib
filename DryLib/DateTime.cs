3974c3c0e5a5973be89a629e186cb5da725b5a63
Markus Malz <markus.malz.ext@siemens.com>
2021-10-05 12:39:01 +0200

    Added DateTime methods to generate folder and filenames

---
+++ DryLib/DateTime.cs
@@ -0,0 +1,19 @@
+public class DateTimeHelper
+{
+    public static string GetDateTimeNowForFilesystem()
+	{
+		var now = DateTime.Now;
+		return $"{now.Year}-{now.Month.ToString().PadLeft(2,'0')}-{now.Day.ToString().PadLeft(2,'0')}T{now.Hour.ToString().PadLeft(2,'0')}{now.Minute.ToString().PadLeft(2,'0')}{now.Second.ToString().PadLeft(2,'0')}";
+	}
+
+	public static string GetDateNowForFilesystem()
+	{
+		var now = DateTime.Now;
+		return $"{now.Year}-{now.Month.ToString().PadLeft(2, '0')}-{now.Day.ToString().PadLeft(2, '0')}";
+	}
+
+	public static string GetDateNowForFilesystem(DateTime dateTime)
+	{		
+		return $"{dateTime.Year}-{dateTime.Month.ToString().PadLeft(2, '0')}-{dateTime.Day.ToString().PadLeft(2, '0')}";
+	}
+}
